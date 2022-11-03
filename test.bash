4b18c932495a268a8892bdbc4681af85460b1082
ryoma0112 <s21cu1126@s.chibakoudai.jp>
2022-11-03 12:20:59 +0900

    Add test.yml

--- test.bash
+++ test.bash
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-seq 5 | ./plus
+ng () {
+	echo NG at Line $1
+	rea=1
+}
+
+res=0 
+
+### I/O test ###
+out=$(seq 5 | .plus)
+[ "${out} = 14 "] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+
+exit $res
 
